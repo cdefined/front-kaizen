030e137e4ac856a2b4dbb8fac970b5fbb2957f55
KARINO Keita <129916226+Mr-Kyary-at-eg@users.noreply.github.com>
2025-07-22 17:35:48 +0900

    [Mr-Kyary-at-eg:feat/lesson1] - オリジナルのファイルを元に戻す。リンター？での修正をrevert。

--- typescript-lessons/lesson_1/homework/homework.ts
+++ typescript-lessons/lesson_1/homework/homework.ts
@@ -11,7 +11,7 @@ function formatZanpakuto(zanpakuto: Zanpakuto): string {
 // 2. filterByType
 function filterByType(
   zanpakutos: Zanpakuto[],
-  targetType: Zanpakuto["type"]
+  targetType: Zanpakuto["type"],
 ): Zanpakuto[] {
   // TODO: 実装
   throw new Error("Not implemented");
@@ -19,7 +19,7 @@ function filterByType(
 
 // 3. hasBankai (type guard)
 function hasBankai(
-  zanpakuto: Zanpakuto
+  zanpakuto: Zanpakuto,
 ): zanpakuto is Zanpakuto & { bankai: string } {
   // TODO: 実装
   throw new Error("Not implemented");
@@ -39,12 +39,12 @@ console.log("formatZanpakuto テスト:");
 console.assert(
   formatZanpakuto(zanpakutoCollection[0]) ===
     "Zangetsu (Kurosaki Ichigo) - Melee Type [Released]",
-  "formatZanpakuto failed for released zanpakuto"
+  "formatZanpakuto failed for released zanpakuto",
 );
 console.assert(
   formatZanpakuto(zanpakutoCollection[1]) ===
     "Hyorinmaru (Hitsugaya Toshiro) - Elemental Type [Sealed]",
-  "formatZanpakuto failed for sealed zanpakuto"
+  "formatZanpakuto failed for sealed zanpakuto",
 );
 
 // filterByType テスト
@@ -52,22 +52,22 @@ console.log("filterByType テスト:");
 const meleeZanpakutos = filterByType(zanpakutoCollection, "melee");
 console.assert(
   meleeZanpakutos.length === 4,
-  "filterByType melee count incorrect"
+  "filterByType melee count incorrect",
 );
 console.assert(
   meleeZanpakutos.every((z) => z.type === "melee"),
-  "filterByType melee filter incorrect"
+  "filterByType melee filter incorrect",
 );
 
 // hasBankai テスト
 console.log("hasBankai テスト:");
 console.assert(
   hasBankai(zanpakutoCollection[0]) === true,
-  "hasBankai should return true for Zangetsu"
+  "hasBankai should return true for Zangetsu",
 );
 console.assert(
   hasBankai(zanpakutoCollection[9]) === false,
-  "hasBankai should return false for Haineko"
+  "hasBankai should return false for Haineko",
 );
 
 // getRankings テスト
@@ -76,11 +76,11 @@ const top3 = getRankings(zanpakutoCollection, 3);
 console.assert(top3.length === 3, "getRankings should return exactly 3 items");
 console.assert(
   top3[0].includes("Ryujin Jakka"),
-  "Top ranked should be Ryujin Jakka"
+  "Top ranked should be Ryujin Jakka",
 );
 console.assert(
   top3[0].includes("9500"),
-  "Top ranked should show power level 9500"
+  "Top ranked should show power level 9500",
 );
 
 console.log("=== テスト完了 ===");
